97d2037beabcac457cb97f9875a64d93a2296d95
Natali Shatina <natali.shatina@gmail.com>
2026-05-30 11:56:00 +0200

    working with Form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,48 @@
-console.log('Hallo');
+const form = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (savedData) {
+  formData.email = savedData.email || '';
+  formData.message = savedData.message || '';
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+
+  if (!name) return;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const { email, message } = formData;
+
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = '';
+  formData.message = '';
+
+  form.reset();
+});
